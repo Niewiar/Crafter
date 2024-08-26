fc36837ff9d33c758080f36ec1fd28a24f493c4f
Patryk Niewiarowski <patrykniewiarowski@gmail.com>
2024-08-26 17:10:19 +0200

    Implemented basic player movement

---
+++ Crafter/Assets/Crafter/Input/InputProjectInstaller.cs
@@ -0,0 +1,30 @@
+﻿using Zenject;
+
+namespace Crafter.Input
+{
+    public class InputProjectInstaller : ScriptableObjectInstaller<InputProjectInstaller>
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<InputProjectManager>().AsSingle().NonLazy();
+        }
+
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("Assets/Crafter/Installers/Project/Input Installer", priority = 0)]
+        public static void CreateInstaller()
+        {
+            string directory = "Assets/";
+
+            if (UnityEditor.Selection.objects.Length > 0)
+            {
+                directory = UnityEditor.AssetDatabase.GetAssetPath(UnityEditor.Selection.objects[0]);
+            }
+            
+            InputProjectInstaller instance = CreateInstance<InputProjectInstaller>();
+            instance.name = "Input_Project_Installer";
+            UnityEditor.AssetDatabase.CreateAsset(instance, $"{directory}/{instance.name}.asset");
+            UnityEditor.AssetDatabase.SaveAssets();
+        }
+#endif
+    }
+}
